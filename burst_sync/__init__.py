7686b96b2fd95633850c1e58b36b23a259c3ce29
Eisenman <eisenmanl@neuro.wucon.wustl.edu>
2017-11-15 13:40:08 -0600

    try again to include modified __init__.py

--- burst_sync/__init__.py
+++ burst_sync/__init__.py
@@ -1,7 +1,6 @@
-from ._version import get_versions
-
-__version__ = '0.2.0.Dev0'
-del get_versions
+# -*- coding: utf-8 -*-
 
 from .asdr import asdr
 from .b_stat import b_statistic
+
+__version__ = '0.2.0.Dev0'
